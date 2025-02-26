53644a5aca2fd6a8ae019bfb409fb80f66cd6292
Sophie Jean <363449299@GoTVDSB.ca>
2025-02-26 14:23:27 -0500

    Add project files.

---
+++ Topic 4 - User Input ASSIGNMENT/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Topic_4___User_Input_ASSIGNMENT
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
